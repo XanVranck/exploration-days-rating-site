bef3a4315d5aa236a690049d2be28373d14bec35
XanVranck <vranckaert.xan@gmail.com>
2018-05-14 19:38:49 +0200

    hello world frontje

--- backend/src/main/kotlin/com/cegeka/explorationdays/backend/Controller.kt
+++ backend/src/main/kotlin/com/cegeka/explorationdays/backend/Controller.kt
@@ -1,5 +1,6 @@
 package com.cegeka.explorationdays.backend
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,6 +9,7 @@ class Controller {
 
 
     @GetMapping("/exploration-days-backend")
+    @CrossOrigin("http://localhost:8081")
     fun helloworld(): String {return "hello world"}
 
 
---
+++ frontend/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{helloWorld}}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+
+export default {
+  name: 'app',
+  data:function() {return {helloWorld: String}}, 
+  components: {
+    HelloWorld
+  }, created() {
+            axios({ method: "GET", "url": "http://localhost:8080/exploration-days-backend" }).then(result => {
+                this.helloWorld = result.data;
+            }, error => {
+                console.error(error);
+            });
+
+            return this.helloWorld;
+        },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
